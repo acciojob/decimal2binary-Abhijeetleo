56333e75d5fc1376e8e89afe64b346e0db5ef4f9
Abhijeetleo <134223652+Abhijeetleo@users.noreply.github.com>
2023-07-27 19:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let res = "";
+		while(num>0)
+			{
+				let d = num%2;
+				res = d + res;
+				num = num/2;
+			}
+  return res;
 }
 
 window.decimalToBinary = decimalToBinary;
